90bdd55c84a4b1d1938397efee62414e8fea784b
MICHAELKITH <michaelkithinji27@gmail.com>
2024-10-17 23:58:02 +0300

    Understanding apis

--- backend/main.go
+++ backend/main.go
@@ -10,7 +10,7 @@ func main() {
     app := fiber.New()
 
     app.Get("/api", func (c *fiber.Ctx) error {
-        return c.SendString("Hello, Backend")
+        return c.SendString("Hello, Backend Server")
     })
 
     log.Fatal(app.Listen(":3000"))
